5db929f4bfb61fd0c32ea8c4a336637a697ae139
Pratham41 <kambleprathmesh41@gmail.com>
2024-04-25 09:55:44 +0530

    Remove comments

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,5 @@
 const Pool = require("pg").Pool;
 
-// process.env.PGHOST = 'openly-workable-longhorn-iad.a1.pgedge.io';
-// process.env.PGUSER = 'admin';
-// process.env.PGDATABASE = 'defaultdb';
-// process.env.PGSSLMODE = 'require';
-// process.env.PGPASSWORD = '****************';
 const pool = new Pool({
     user: process.env.PGUSER,
     password: process.env.PGPASSWORD,
